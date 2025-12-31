282e089559046074a7534502289e7e0887b94736
Linga Raj M <lingaraj501@gmail.com>
2025-12-31 17:06:11 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,1 +1,28 @@
-
+module.exports = {
+  title: 'Technical Writing Portfolio',
+  tagline: 'Documentation by Lingraj M',
+  url: 'https://lingaraj-tw.github.io',
+  baseUrl: '/Technical-Writing-Portfolio/',
+  favicon: 'img/favicon.ico',
+ 
+  organizationName: 'lingaraj-tw',
+  projectName: 'Technical-Writing-Portfolio',
+ 
+  presets: [
+    [
+      'classic',
+      {
+        docs: { sidebarPath: require.resolve('./sidebars.js') },
+        theme: { customCss: require.resolve('./src/css/custom.css') },
+      },
+    ],
+  ],
+ 
+  themeConfig: {
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `© ${new Date().getFullYear()} Lingraj M`,
+    },
+  },
+};
